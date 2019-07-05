c278b08e65b5aee9439a9a78f82f95505276a9d2
soheila <soheilalotfi@ymail.com>
2019-07-05 15:49:32 -0400

    calculate  minutes till next train

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -22,16 +22,36 @@ var nextarrival = "";
 var frequency=0;
 var minutesAway=0;
 
-// when the user clicks the sumit button, 
+// when the user clicks the submit button, 
 //grab the user inputs and write it on the firebase database
 
 $("#add-user").click(function(event){
 
-    event.preventDefault();
-  
+    event.preventDefault();     
+
+// grab the user input
     var trainName=$("#train-name-input").val().trim();
     var destination=$("#destination-input").val().trim();
     var firstTime=$("#time-input").val().trim();
     var tFrequency=$("#frequency-input").val().trim();
-  
+
+//calculate  minutes till next train
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + currentTime.format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+// ---------------------------------------------------------------      
+
 });
